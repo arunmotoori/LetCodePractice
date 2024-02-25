20389bd7d0e23e4af2dda11879b8c676033182c7
Arun Motoori <arun.motoori@gmail.com>
2024-02-25 08:22:02 +0530

    Makemytrip Search flights part 2

--- src/test/java/makemytripone/SearchFlighs.java
+++ src/test/java/makemytripone/SearchFlighs.java
@@ -13,14 +13,17 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class SearchFlighs {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		
 		WebDriver driver = new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.get("https://www.makemytrip.com/");
 		
 		WebDriverWait wait = new WebDriverWait(driver,Duration.ofSeconds(10));
-		WebElement fromCityPlace = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("fromCity")));
+		WebElement roundTripOption = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//li[@data-cy='roundTrip']")));
+		roundTripOption.click();
+		
+		WebElement fromCityPlace = driver.findElement(By.id("fromCity"));
 		fromCityPlace.click();
 		
 		WebElement fromField = driver.findElement(By.xpath("//input[@placeholder='From']"));
@@ -40,7 +43,19 @@ public class SearchFlighs {
 		actions.pause(Duration.ofSeconds(2)).sendKeys(Keys.ARROW_DOWN)
 		.sendKeys(Keys.ARROW_DOWN).sendKeys(Keys.ARROW_DOWN)
 		.sendKeys(Keys.ENTER).build().perform();
+	
 		
+		WebElement startDate = driver.findElement(By.xpath("(//div[@aria-selected='true'][@aria-disabled='false'])[1]"));
+		startDate.click();
+		
+
+		/*
+		WebElement endDate = driver.findElement(By.xpath("(//div[not(contains(@class,'DayPicker-Day--outside'))][contains(@class,'DayPicker-Day--selected')])[2]"));
+		endDate.click();
+		*/
+		
+		WebElement endDate = driver.findElement(By.xpath("(//div[@class='DayPicker-Day'])[1]"));
+		endDate.click();
 		
 		
 		
